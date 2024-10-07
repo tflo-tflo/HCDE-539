0f6cbc11c6b4283442effa591f4f64201ca2f211
tflo <florencet@gmail.com>
2024-10-06 17:34:22 -0700

    Re-add HTML minifier to build process

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const convertImage = require('./convert-HEIC.js'); 
 const convertMD = require('./convert-md.js');
-// const minify = require('./minify-HTML.js'); Messes with the actual content of the file. 
+const minify = require('./minify-HTML.js');
